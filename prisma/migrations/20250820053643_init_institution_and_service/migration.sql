52720ab8c9ad9b83c1cbdc64396a1273077531d7
Faruuuqqq <faruqmahdison@gmail.com>
2025-08-20 20:27:17 +0700

    feat: add Prisma module and service with initial configuration and tests

---
+++ prisma/migrations/20250820053643_init_institution_and_service/migration.sql
@@ -0,0 +1,24 @@
+-- CreateTable
+CREATE TABLE `Institution` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `address` VARCHAR(191) NULL,
+    `phone` VARCHAR(191) NULL,
+    `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updatedAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Service` (
+    `id` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NOT NULL,
+    `location` VARCHAR(191) NULL,
+    `institutionId` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Service` ADD CONSTRAINT `Service_institutionId_fkey` FOREIGN KEY (`institutionId`) REFERENCES `Institution`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
